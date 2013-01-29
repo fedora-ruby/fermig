f86f1523e98c277c4e13d475bd7184f62d726d3e
Vít Ondruch <vondruch@redhat.com>
2013-01-29 14:51:36 +0100

    Add script updating entire .spec file for mass rebuild.

---
+++ update.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+ROOT = File.expand_path(File.dirname(__FILE__))
+
+F19 = File.join ROOT, 'f19.rb'
+CHANGELOG = File.join ROOT, 'changelog.rb'
+
+modified = `#{F19} < "#{ARGV[0]}"`
+File.open(ARGV[0], "w") { |f| f.puts modified }
+
+`#{CHANGELOG} "#{ARGV[0]}"`
