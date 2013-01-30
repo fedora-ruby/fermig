327a4f154be6a0ed290a5ccade435db43e342858
Vít Ondruch <vondruch@redhat.com>
2013-01-30 11:39:46 +0100

    Remove unnecessary input redirect.

--- update.rb
+++ update.rb
@@ -5,7 +5,7 @@ ROOT = File.expand_path(File.dirname(__FILE__))
 F19 = File.join ROOT, 'f19.rb'
 CHANGELOG = File.join ROOT, 'changelog.rb'
 
-modified = `#{F19} < "#{ARGV[0]}"`
+modified = `#{F19} "#{ARGV[0]}"`
 File.open(ARGV[0], "w") { |f| f.puts modified }
 
 `#{CHANGELOG} "#{ARGV[0]}"`
