a8a13cc0bdf43958ea4dd5e4e0a61a76c21f5a04
Vít Ondruch <vondruch@redhat.com>
2013-02-18 16:49:11 +0100

    Migrate 'gem install' command to %gem_install macro.

--- f19.rb
+++ f19.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 changelog = false
+gem_install = false
 
 ARGF.lines do |line|
   changelog = line =~ /%changelog/
@@ -9,12 +10,28 @@ ARGF.lines do |line|
     # Remove the rubyabi macro definition.
     next if line =~ /%global.*?ruby.*?abi/
 
+    # Configuration flags are part of install macro now.
+    next if line =~ /export CONFIGURE_ARGS="--with-cflags='%{optflags}'"/
+
     # Drop the hard coded ruby(abi) dependency.
     line.gsub!(/(ruby.*?abi.*?) .*=.*/, '\1')
     line.chomp!
 
     # Rename ruby(abi) to ruby(release).
     line.gsub!(/(ruby.*?)abi(.*?)/, '\1release\2')
+
+    # Replace custom 'gem install' command with %gem_install macro.
+    if line =~ /gem\s*install.*/
+      pre = line[/(.*)gem\s*install.*/, 1]
+      gem_name ||= line[/%{SOURCE.*}/,0]
+      while line =~ /\\$/
+        line = gets
+        gem_name ||= line[/%{SOURCE.*}/,0]
+      end
+
+      line = "#{pre}%gem_install"
+      line += " -n #{gem_name}" if gem_name
+    end
   end
 
   puts line
