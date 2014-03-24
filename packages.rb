49895f10b8e92f14ece3fa394f6f86aa871de181
Vít Ondruch <vondruch@redhat.com>
2014-03-24 11:57:19 +0100

    Make package list more robust.

--- packages.rb
+++ packages.rb
@@ -8,7 +8,8 @@ packages = `repoquery --repoid=rawhide-source --arch=src --whatrequires 'ruby*'`
 exit $?.to_i if $?.to_i != 0
 
 packages = packages.lines.to_a
-packages.map! { |package| package[/(.*)-.*?-.*?/, 1] }
+packages.map! { |package| package.strip[/(.*)-.*?-.*?/, 1] }
+packages.compact!
 packages.uniq!
 packages.sort!
 
