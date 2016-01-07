434a22c1eac0a32f22478e67828be28d83cbf4f9
Pavel Valena <pvalena@redhat.com>
2016-01-07 18:03:19 +0100

    Replace shell code with Ruby.

--- userpackages.rb
+++ userpackages.rb
@@ -4,15 +4,21 @@ ROOT = File.expand_path(File.dirname(__FILE__))
 
 PACKAGES = File.join ROOT, 'packages.rb'
 
-userpackages = `pkgdb-cli list --user '#{ENV['USER']}' | tr -s ' ' | grep -vE '^Total: ' | cut -d' ' -f2`
+userpackages = `pkgdb-cli list --user "$USER"`
 exit $?.to_i if $?.to_i != 0
 
-packages = `exec #{PACKAGES}`
+userpackages = userpackages.lines
 
-userpackages = userpackages.lines & packages.lines
+userpackages.map! { |pkg| pkg.lstrip.split(' ')[0] + "\n" }
+
+packages = `#{PACKAGES}`
+exit $?.to_i if $?.to_i != 0
+
+userpackages &= packages.lines
 
 userpackages.sort!
 userpackages.uniq!
 userpackages.compact!
+userpackages.delete('\n')
 
 puts userpackages
