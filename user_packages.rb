4ebdcf3f4682b4687c321babea6a6059b5d77f2e
Vít Ondruch <vondruch@redhat.com>
2018-01-03 17:06:33 +0100

    Use dist-git Pagure api to retrieve list of user packages.
    PkgDB and pkgdb-cli were deprecated.

--- user_packages.rb
+++ user_packages.rb
@@ -1,20 +1,24 @@
 #!/usr/bin/ruby
 
+require 'open-uri'
+require 'json'
+
 ROOT = File.expand_path(File.dirname(__FILE__))
 
 PACKAGES = File.join ROOT, 'packages.rb'
 
-user_packages = `pkgdb-cli list --user "$USER"`
-exit $?.to_i if $?.to_i != 0
+PKGS_API_URL = "https://src.fedoraproject.org/api/0/projects?username=#{ENV['USER']}"
 
-user_packages = user_packages.lines
-
-user_packages.map! { |pkg| "#{pkg[/^\s+([\w\-_]+)\s+/, 1]}\n" }
+user_packages = JSON.parse(open(PKGS_API_URL).read)["projects"].map do |proj|
+  proj["name"]
+end
 
 packages = `#{PACKAGES}`
 exit $?.to_i if $?.to_i != 0
 
-user_packages &= packages.lines
+packages = packages.lines.map { |l| l.strip }
+
+user_packages &= packages
 
 user_packages.sort!
 user_packages.uniq!
