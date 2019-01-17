ab80e4ef15bebc3365a24f11bbeed16ecf18fc78
Vít Ondruch <vondruch@redhat.com>
2019-01-17 11:57:17 +0100

    Add support for Pagure pagination.

--- user_packages.rb
+++ user_packages.rb
@@ -9,14 +9,25 @@ PACKAGES = File.join ROOT, 'packages.rb'
 
 PKGS_API_URL = "https://src.fedoraproject.org/api/0/projects?username=#{ENV['USER']}"
 
-user_packages = JSON.parse(open(PKGS_API_URL).read)["projects"].map do |proj|
-  proj["name"]
+page = String.new(PKGS_API_URL)
+
+user_packages = []
+
+while page do
+  json = JSON.parse(open(page).read)
+
+  page = json.dig('pagination', 'next')
+
+  user_packages += json ["projects"].map do |proj|
+    proj["name"]
+  end
 end
 
 packages = `#{PACKAGES}`
 exit $?.to_i if $?.to_i != 0
 
-packages = packages.lines.map { |l| l.strip }
+packages = packages.lines
+packages.map!(&:strip)
 
 user_packages &= packages
 
