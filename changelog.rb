bad3ee03b6461da9c7da5af33f0d96510bcafb98
Vít Ondruch <vondruch@redhat.com>
2020-01-16 09:56:30 +0100

    F32 modifications.

--- changelog.rb
+++ changelog.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
 
-COMMENT = %{Rebuilt for https://fedoraproject.org/wiki/Changes/Ruby_2.6}
+COMMENT = %{Rebuilt for https://fedoraproject.org/wiki/Changes/Ruby_2.7}
 
 `rpmdev-bumpspec -c "#{COMMENT}" "#{ARGV[0]}"`
