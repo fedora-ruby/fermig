eb66e53ebaee9784218ffd76445467aef7f4936a
Vít Ondruch <vondruch@redhat.com>
2024-01-02 17:35:34 +0100

    F40 modifications.

--- changelog.rb
+++ changelog.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
 
-COMMENT = %{Rebuilt for https://fedoraproject.org/wiki/Changes/Ruby_3.2}
+COMMENT = %{Rebuilt for https://fedoraproject.org/wiki/Changes/Ruby_3.3}
 
 `rpmdev-bumpspec -D -c "#{COMMENT}" "#{ARGV[0]}"`
